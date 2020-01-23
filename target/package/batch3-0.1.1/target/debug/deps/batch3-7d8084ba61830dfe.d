3f3103c1ef9b9efa3b0ee0396715805be5584a9a
Muhammad Adnan <muhammad.adnan1990@gmail.com>
2020-01-23 13:44:41 +0500

    Readme file updated

---
+++ target/package/batch3-0.1.1/target/debug/deps/batch3-7d8084ba61830dfe.d
@@ -0,0 +1,7 @@
+/Users/adnan/Desktop/batch3/target/package/batch3-0.1.1/target/debug/deps/batch3-7d8084ba61830dfe.rmeta: src/lib.rs
+
+/Users/adnan/Desktop/batch3/target/package/batch3-0.1.1/target/debug/deps/libbatch3-7d8084ba61830dfe.rlib: src/lib.rs
+
+/Users/adnan/Desktop/batch3/target/package/batch3-0.1.1/target/debug/deps/batch3-7d8084ba61830dfe.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/package/batch3-0.1.1/target/debug/libbatch3.d
@@ -0,0 +1,1 @@
+/Users/adnan/Desktop/batch3/target/package/batch3-0.1.1/target/debug/libbatch3.rlib: /Users/adnan/Desktop/batch3/target/package/batch3-0.1.1/src/lib.rs
